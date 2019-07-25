e07178d75978b2c991f9236f47c2b5536a0a498c
Eka <ekaone3033@gmail.com>
2019-07-25 13:00:20 +0700

    Led Builtin

---
+++ LED/LedBuiltin.js
@@ -0,0 +1,20 @@
+/**
+ * http://johnny-five.io/examples/led/
+ *  
+ * */
+
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  this.repl.inject({
+    led: led
+  });
+
+  led.blink();
+});
